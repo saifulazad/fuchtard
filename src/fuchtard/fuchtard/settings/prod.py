447d0081d40e37edafa46e3decb3134f3fe10eeb
robox <roboxv@gmail.com>
2016-07-10 01:39:05 +0700

    moving custom admin email handler to production settings

--- src/fuchtard/fuchtard/settings/prod.py
+++ src/fuchtard/fuchtard/settings/prod.py
@@ -11,3 +11,22 @@ ALLOWED_HOSTS = [
 SPARKPOST_API_KEY = 'fc52712a253df3d28d69fdb10fd521271c36eb09'
 FUCHTARD_ORDERS_EMAIL = 'order@maxisushi.kz'
 FUCHTARD_NOREPLY_EMAIL = 'Maxi Sushi <noreply@maxisushi.kz>'
+
+# LOGGING
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'mail_admins': {
+            'class': 'main.utils.CustomAdminEmailHandler',
+            'level': 'ERROR',
+            # 'filters': ['special']
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            # 'propagate': True,
+        },
+    },
+}
