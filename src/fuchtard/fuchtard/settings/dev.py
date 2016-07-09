b1ce0a8501125869bfc6154936a4c07b23ca17c6
robox <roboxv@gmail.com>
2016-07-10 02:42:30 +0700

    notify in telegram channel about new orders

--- src/fuchtard/fuchtard/settings/dev.py
+++ src/fuchtard/fuchtard/settings/dev.py
@@ -10,6 +10,10 @@ SPARKPOST_API_KEY = 'fc52712a253df3d28d69fdb10fd521271c36eb09'
 FUCHTARD_ORDERS_EMAIL = 'roboxv+fuchtardorder@gmail.com'
 FUCHTARD_NOREPLY_EMAIL = 'Maxi Sushi <noreply@maxisushi.kz>'
 
+# TELEGRAM BOT
+TELEGRAM_BOT_API_TOKEN = '248042210:AAF7O2ryYuhxIvqegx3U0pEPrPTUrgPmvFA'
+TELEGRAM_BOT_NOTIFICATION_CHANNEL_ID = '119702721'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
